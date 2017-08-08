8067ea6df96fddbe73ad38571a00bfc2738aaa8b
Louis Burkhardt <LouisDotCom@users.noreply.github.com>
2017-08-08 11:47:56 -0600

    second tab: displays Items

--- NextItem/SecondViewController.swift
+++ NextItem/SecondViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var ItemTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(Items.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let prototype = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier: "prototype")
+        prototype.textLabel?.text = Items[indexPath.row]
+        return(prototype)
+        
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
